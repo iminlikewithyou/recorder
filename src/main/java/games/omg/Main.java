1089cdf51ef6ffc94ec28bfc3f4059e82cecff4f
iminlikewithyou <christian@omg.games>
2023-12-07 17:54:06 -0600

    create test thing for ProtocolLib

--- src/main/java/games/omg/Main.java
+++ src/main/java/games/omg/Main.java
@@ -5,6 +5,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+
 public class Main extends JavaPlugin implements Listener {
 	
 	@EventHandler
@@ -15,5 +21,20 @@ public class Main extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
+
+		ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
+
+        protocolManager.addPacketListener(new PacketAdapter(this, PacketType.Play.Client.BLOCK_DIG, PacketType.Play.Server.BLOCK_CHANGE) {
+            @Override
+            public void onPacketReceiving(PacketEvent event) {
+								event.getPlayer().sendMessage("PACKET " + event.getPacketType());
+            }
+
+						@Override
+						public void onPacketSending(PacketEvent event) {
+								event.getPlayer().sendMessage("PACKET " + event.getPacketType());
+						}
+        });
+
 	}
 }
